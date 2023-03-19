75a687edcabfa35eef0c98cab822fbdddbaa28d3
Segun Ajibola <ajbl.segun@gmail.com>
2023-03-19 02:01:00 +0100

    first commit

--- app/page.jsx
+++ app/page.jsx
@@ -1,16 +1,16 @@
 "use client";
 
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
-import styles from './page.module.css'
-import Hero from "../components/Hero"
+import Image from "next/image";
+import { Inter } from "next/font/google";
+import styles from "./page.module.css";
+import Hero from "../components/Hero";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   return (
     <>
-    <Hero />
+      <Hero />
     </>
-  )
+  );
 }
