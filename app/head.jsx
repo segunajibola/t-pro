65d4c6362f35d1e6dabb0961677977471ab6818a
Segun Ajibola <ajbl.segun@gmail.com>
2023-03-19 02:39:52 +0100

    first commit

---
+++ app/head.jsx
@@ -0,0 +1,10 @@
+export default function Head() {
+  return (
+    <>
+      <title>T-pro signature</title>
+      <meta content="width=device-width, initial-scale=1" name="viewport" />
+      <meta name="description" content="T-pro signature, bead making, event planner, make-up artist" />
+      <link rel="icon" href="/favicon.ico" />
+    </>
+  )
+}
--- app/layout.jsx
+++ app/layout.jsx
@@ -5,11 +5,6 @@ import { useState } from "react";
 import Navbar from "@/components/Navbar";
 import Footer from "@/components/Footer";
 
-export const metadata = {
-  title: "T-pro signature",
-  description: "T-pro signature, bead making, event planner, make-up artist",
-};
-
 export default function RootLayout({ children }) {
   const [darkMode, setDarkMode] = useState(false);
   const handleMode = () => setDarkMode(!darkMode);
