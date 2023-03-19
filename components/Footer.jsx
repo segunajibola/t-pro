84afc581ad7c5eac836d5f5c4b8f2894819053a2
Segun Ajibola <ajbl.segun@gmail.com>
2023-03-19 01:38:48 +0100

    first commit

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -8,22 +8,22 @@ const Footer = () => (
     variants={footerVariants}
     initial="hidden"
     whileInView="show"
-    className="py-8 relative"
+    className="py-4 bg-gray-200 dark:bg-gray-500 text-gray-700 dark:text-gray-300 relative"
   >
     <div className="footer-gradient" />
-    <div className="mx-auto flex flex-col gap-8 bg-black">
+    <div className="mx-auto flex flex-col gap-8">
       <div className="flex flex-col">
 
-        <div className="mb-[50px] h-[2px] bg-white opacity-10" />
+        <div className="mb-[30px] h-[2px] bg-gray-500 opacity-10" />
 
-        <div className="flex items-center justify-between flex-wrap gap-4 ">
-          <h4 className="font-extrabold text-[24px] text-white">
+        <div className="flex items-center justify-between flex-wrap gap-4 p-6">
+          <h4 className="font-bold text-[24px]">
             T-PRO
           </h4>
-          <p className="font-normal text-[18px] text-white">
-            Made with ❤, <a href="https://www.nextjs.org" className='underline'>Nextjs</a>  and <a href="https://tailwindcss.com" className='underline'>TailwindCSS</a> by  <a href="https://www.segunajibola.com" className='underline'>Segun Ajibola.</a>
+          <p className="font-normal text-[18px]">
+            Made with ❤ by  <a href="https://www.segunajibola.com" className='underline'>Segun Ajibola.</a>
           </p>
-          <p className="font-normal text-[14px] text-white opacity-50">
+          <p className="font-normal text-[14px] opacity-50">
             Copyright © {new Date().getFullYear()} T-pro Signature. All rights reserved.
           </p>
         </div>
