4e716a19b48f7466cd1f62261293ea185c62f564
Segun Ajibola <ajbl.segun@gmail.com>
2023-03-29 00:46:58 +0100

    service

--- app/[serviceName]/page.jsx
+++ app/[serviceName]/page.jsx
@@ -3,10 +3,9 @@ import React from "react";
 const ParticularService = ({ params }) => {
   const { serviceName } = params;
 
-  const newServiceName = serviceName.split("-").join(" ")
-  word.charAt(0).toUpperCase()
-  + word.slice(1)
+  const splitStr = serviceName.split("-").join(" ")
   
+  const newServiceName = splitStr.charAt(0).toUpperCase() + splitStr.slice(1)  
 
   return <div className="mt-20">{newServiceName}</div>;
 };
