239147cbb273a4f23d973b459cc164020e3d8552
Segun Ajibola <ajbl.segun@gmail.com>
2023-03-29 02:39:16 +0100

    service

--- sections/About.jsx
+++ sections/About.jsx
@@ -11,29 +11,39 @@ const About = () => (
     className={`${styles.paddings} md:py-24 bg-gray-200 dark:bg-gray-600 relative z-10`}
   >
     <div className="gradient-02 z-0" />
+
+    <div className="my-5">
+      <TypingText
+        title="| About T-pro Signature"
+        textStyles="text-center md:text-[24px] font-bold dark:text-gray-200"
+      />
+    </div>
+
     <div className="flex sm:flex-col md:flex-row">
       <motion.div
         variants={staggerContainer}
         initial="hidden"
         whileInView="show"
         viewport={{ once: false, amount: 0.25 }}
-        className={`mx-auto ${styles.flexCenter} border-4 border-red-500 flex-col w-1/2`}
+        className={`mx-auto ${styles.flexCenter} flex-col w-1/2 text-right`}
       >
-        <TypingText
-          title="| About T-pro Signature"
-          textStyles="text-center md:text-[30px] font-bold dark:text-gray-200"
-        />
-
+        <motion.h2
+        variants={fadeIn("up", "tween", 0.1, 0.3)}
+        className="text-[19px] md:text-[20px]"
+        >
+          T-Pro Signature: The Art Of Beauty
+        </motion.h2>
         <motion.p
           variants={fadeIn("up", "tween", 0.1, 0.3)}
-          className="mt-[8px] font-normal text-[14px] md:text-[24px] text-center text-secondary-white dark:text-white"
+          className="mt-[8px] font-normal mr-3 text-[17px] md:text-[20px] text-secondary-white dark:text-white"
         >
           Welcome to our world of beauty, creativity, and elegance! Our website
           offers a wide range of services in beauty, bead making, and event
           decoration that are tailored to your specific needs and preferences.
           We are committed to providing you with the best quality products and
           services that will leave you feeling and looking your absolute best.
-          At our website, we pride ourselves on our exceptional customer service and attention to detail.
+          At our website, we pride ourselves on our exceptional customer service
+          and attention to detail.
           {/*  We understand that every customer has
           different needs and preferences, and we strive to exceed your
           expectations every time. Whether you're looking for a simple service
@@ -41,23 +51,19 @@ const About = () => (
           desired look and feel. Thank you for choosing our website for all your
           beauty, bead making, and event decoration needs! */}
         </motion.p>
-
-        
       </motion.div>
-      <div className="flex flex-col justify-center w-[50%] border-4 border-green-500">
-      <img src="/wedding-makeup.jpg" alt="" className="h-[90%] w-[100%]" />
+      <div className="flex flex-col justify-center w-[50%]">
+        <img src="/wedding-makeup.jpg" alt="" className="h-[90%] w-[100%]" />
       </div>
-        
     </div>
     <div className="flex justify-center">
       <motion.img
-          variants={fadeIn("up", "tween", 0.1, 0.3)}
-          src="/arrow-down.svg"
-          alt="arrow down"
-          className="w-[50px] h-[50px] object-contain my-[28px] bg-gray-300 p-3 dark:bg-gray-500"
-        />
+        variants={fadeIn("up", "tween", 0.1, 0.3)}
+        src="/arrow-down.svg"
+        alt="arrow down"
+        className="w-[50px] h-[50px] object-contain my-[28px] bg-gray-300 p-3 dark:bg-gray-500"
+      />
     </div>
-    
   </section>
 );
 
