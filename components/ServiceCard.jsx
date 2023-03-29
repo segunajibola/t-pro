0e337f379eb3330ef27319d6debbbebcebdc61c6
Segun Ajibola <ajbl.segun@gmail.com>
2023-03-29 23:53:45 +0100

    service

--- components/ServiceCard.jsx
+++ components/ServiceCard.jsx
@@ -4,13 +4,13 @@ import Link from "next/link";
 const ServiceCard = ({ service }) => {
   return (
     <div
-      className="flex flex-col bg-gray-400 rounded-3xl shadow-lg h-full relative"
+      className="flex flex-col bg-gray-400 rounded-3xl shadow-lg h-full relative mx-4 md:mx-2"
       key={service.id}
     >
       <img
         src={service.image}
         alt={`${service.name} services`}
-        className="rounded-t-2xl object-cover object-top w-full h-40"
+        className="rounded-t-2xl object-cover object-top w-full h-48"
       />
       <div className="flex flex-col px-3 py-5 text-center h-full justify-between">
         <h3 className="font-bold text-2xl">{service.name}</h3>
