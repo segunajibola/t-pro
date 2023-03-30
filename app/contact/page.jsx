e1a7e25661f1d03d165b43b056db77721473a3e8
Segun Ajibola <ajbl.segun@gmail.com>
2023-03-30 23:12:48 +0100

    service

--- app/contact/page.jsx
+++ app/contact/page.jsx
@@ -32,55 +32,29 @@ const MainContact = () => (
       >
         Reach Us
       </motion.p>
-      </div>
-      <div className="mt-10 text-center">
+    </div>
+    <div className="mt-10">
       <p className="w-[80%] mx-auto">
-        We are committed to providing you with the best quality products and
-        services that will leave you feeling and looking your absolute best. At
-        our website, we pride ourselves on our exceptional customer service and
-        attention to detail. We understand that every customer has different
-        needs and preferences, and we strive to exceed your expectations every
-        time. Whether you're looking for a simple service or a complete
-        transformation, we are here to help you achieve your desired look and
-        feel. Thank you for choosing our website for all your beauty, bead
-        making, and event decoration needs!
+        We would love to hear from you! Our team is dedicated to providing you
+        with exceptional customer service and support. If you have any
+        questions, comments, or concerns, please feel free to reach out to us
+        using the contact information below:
       </p>
+
+      <div className="my-10 flex flex-col gap-3">
+        <p>Email: [insert email address here]</p>
+        <p>Phone: [insert phone number here]</p>
+        <p>Address: [insert physical address here]</p>
       </div>
-      <div className="flex flex-col justify-center items-center">
-      <motion.p
-        variants={fadeIn("up", "tween", 0.2, 1)}
-        className="mt-[20px] mb-[12px] font-normal text-[14px] md:text-[24px] text-center text-secondary-white dark:text-white"
-      >
-        Founder
-      </motion.p>
-      <Image
-        src="/decoration.jpg"
-        alt=""
-        width={400}
-        height={400}
-        className=""
-      />
-      <h2 className="my-6">Aminat Quadri</h2>
-      <p className="w-[80%] mx-auto text-center">
-        Aminat Quadri is Co-Owner and head of operations at Studio Sashiko.
-        Kyle has worked within the Tattoo industry since 2007, receiving a Body
-        Piercing apprenticeship. He most recently acted as manager at a shop
-        practicing body piercing, before opening Studio Sashiko with his wife,
-        Shaughnessy. Kyle’s creative direction with Studio Sashiko is highly
-        influenced by a passion for high fashion, street fashion, design and
-        architecture. His ability to spot and embrace creative trends has
-        allowed Studio Sashiko to realize itself not only as a leader in quality
-        and hyper-realistic work, but as a brand that is trail-blazing within
-        the industry.
+
+      <p>
+        Alternatively, you can also use the contact form below to send us a
+        message directly. Please provide as much detail as possible so that we
+        can assist you in the best way possible. We strive to respond to all
+        inquiries within 24-48 hours. Thank you for choosing our website for
+        your needs. We value your feedback and look forward to hearing from you
+        soon!
       </p>
-      <button className="bg-gray-100 p-3 my-3 dark:text-white rounded-md">
-          <a
-            href=""
-            className=""
-          >
-            Quick call
-          </a>
-        </button>
     </div>
   </div>
 );
