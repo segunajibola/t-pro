c7fff35bfa52c8fb1a0a3a52ec78752fdfa01627
Segun Ajibola <ajbl.segun@gmail.com>
2023-03-30 15:27:15 +0100

    service

--- sections/Navbar.jsx
+++ sections/Navbar.jsx
@@ -56,7 +56,7 @@ const Navbar = ({ handleMode, darkMode }) => {
           </li>
           <span>|</span>
           <li className="cursor-pointer">
-            <Link href="/resources">Make-up</Link>
+            <Link href="/make-up">Make-up</Link>
           </li>
           <span>|</span>
           <li className="cursor-pointer">
