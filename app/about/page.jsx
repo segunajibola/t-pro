8a4ef7676f36cd16f818a51e6b9813f1fb18a63c
Segun Ajibola <ajbl.segun@gmail.com>
2023-03-30 14:59:59 +0100

    service

--- app/about/page.jsx
+++ app/about/page.jsx
@@ -9,10 +9,10 @@ import { fadeIn, staggerContainer } from "../../utils/motion";
 import Image from "next/image";
 
 const MainAbout = () => (
-  <div className={`${styles.innerWidth}`}>
-    <div className="flex gap-4 justify-between">
-      <Image src="/makeup.jpg" alt="" width={200} height={200} className="" />
-      <Image src="/decoration.jpg" alt="" width={200} height={200} className="" />
+  <div className={`${styles.innerWidth} w-[80%] mx-auto pt-24 pb-12`}>
+    <div className="flex gap-4 justify-center">
+      <Image src="/makeup.jpg" alt="" width={400} height={200} className="" />
+      <Image src="/decoration.jpg" alt="" width={400} height={200} className="" />
     </div>
   </div>
 );
