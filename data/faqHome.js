c3227967a85ae5fee1920f5a91cf5a496cde9edb
Segun Ajibola <ajbl.segun@gmail.com>
2023-04-06 01:33:40 +0100

    service

--- data/faqHome.js
+++ data/faqHome.js
@@ -1,32 +1,32 @@
 export const faqHome = [
     {
       "id": 1,
-      "question": "What is Lorde's nationality?",
-      "answer": "She holds both the New Zealand and Croatia citizenship."
+      "question": "What services do you offer?",
+      "answer": "Lorem "
     },
     {
       "id": 2,
-      "question": "How did Ella come up with the stage name 'Lorde'?",
+      "question": "How long have you been in business?",
       "answer": "The name comes from her obsession with royalty and aristocracy. She orignally wanted to be Lord, but added an 'e' on it to make it more feminine."
     },
     {
       "id": 3,
-      "question": "How long has Lorde been in the industry?",
+      "question": "What is your process for working with clients?",
       "answer": "She got her first record deal at the age of 9 years old, 2003. So about 23 years in the industry."
     },
     {
       "id": 4,
-      "question": "Who said Lorde is 'The Future of Music'?",
+      "question": "Can you customize your services to meet my specific needs?",
       "answer": "David Bowie. Also happens to be Lorde's musical influencer."
     },
     {
       "id": 5,
-      "question": "Lorde can see the colour of musical notes?",
+      "question": "What forms of payment do you accept?",
       "answer": "Lorde has (sound-to-color) synesthesia, a neurological condition that allows her to see colours when particular musical notes are played."
     },
     {
       "id": 6,
-      "question": "Why Lorde hates Reggae?",
+      "question": "Do you have any special offers or promotions currently available?",
       "answer": "'I hate reggae, reggae makes me feel like I am late for something.'"
     }
   ]  
