be9f0c2c3199689f8721285481cd6ecf0670eb68
Segun Ajibola <ajbl.segun@gmail.com>
2023-04-06 01:34:53 +0100

    service

--- data/faqHome.js
+++ data/faqHome.js
@@ -2,31 +2,31 @@ export const faqHome = [
     {
       "id": 1,
       "question": "What services do you offer?",
-      "answer": "Lorem "
+      "answer": "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eaque tempore quisquam autem, nisi culpa delectus? Maxime earum temporibus incidunt? Totam alias quasi ratione perspiciatis veritatis voluptatibus fuga, dolorem consequuntur cum."
     },
     {
       "id": 2,
       "question": "How long have you been in business?",
-      "answer": "The name comes from her obsession with royalty and aristocracy. She orignally wanted to be Lord, but added an 'e' on it to make it more feminine."
+      "answer": "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eaque tempore quisquam autem, nisi culpa delectus? Maxime earum temporibus incidunt? Totam alias quasi ratione perspiciatis veritatis voluptatibus fuga, dolorem consequuntur cum."
     },
     {
       "id": 3,
       "question": "What is your process for working with clients?",
-      "answer": "She got her first record deal at the age of 9 years old, 2003. So about 23 years in the industry."
+      "answer": "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eaque tempore quisquam autem, nisi culpa delectus? Maxime earum temporibus incidunt? Totam alias quasi ratione perspiciatis veritatis voluptatibus fuga, dolorem consequuntur cum."
     },
     {
       "id": 4,
       "question": "Can you customize your services to meet my specific needs?",
-      "answer": "David Bowie. Also happens to be Lorde's musical influencer."
+      "answer": "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eaque tempore quisquam autem, nisi culpa delectus? Maxime earum temporibus incidunt? Totam alias quasi ratione perspiciatis veritatis voluptatibus fuga, dolorem consequuntur cum."
     },
     {
       "id": 5,
       "question": "What forms of payment do you accept?",
-      "answer": "Lorde has (sound-to-color) synesthesia, a neurological condition that allows her to see colours when particular musical notes are played."
+      "answer": "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eaque tempore quisquam autem, nisi culpa delectus? Maxime earum temporibus incidunt? Totam alias quasi ratione perspiciatis veritatis voluptatibus fuga, dolorem consequuntur cum."
     },
     {
       "id": 6,
       "question": "Do you have any special offers or promotions currently available?",
-      "answer": "'I hate reggae, reggae makes me feel like I am late for something.'"
+      "answer": "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eaque tempore quisquam autem, nisi culpa delectus? Maxime earum temporibus incidunt? Totam alias quasi ratione perspiciatis veritatis voluptatibus fuga, dolorem consequuntur cum."
     }
   ]  
