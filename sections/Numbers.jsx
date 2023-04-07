dd8a3185436d318b44296ab41d79076a99b8d49b
Segun Ajibola <ajbl.segun@gmail.com>
2023-04-07 19:09:38 +0100

    service

--- sections/Numbers.jsx
+++ sections/Numbers.jsx
@@ -12,26 +12,25 @@ const Numbers = () => {
       className={`${styles.paddings} md:py-24 bg-gray-200 dark:bg-gray-600 relative z-10 w-[80%] h-[500px] mx-auto`}
     >
       <div className="gradient-03 z-0" />
-    <motion.div
-      variants={staggerContainer}
-      initial="hidden"
-      whileInView="show"
-      viewport={{ once: false, amount: 0.25 }}
-      className={`${styles.innerWidth} mx-auto ${styles.flexCenter} flex-col`}
-    >
-        <TypingText
-        title="| Metrics"
-        textStyles="text-center md:text-[30px] font-bold dark:text-gray-200"
-      />
-
-      <motion.p
-        variants={fadeIn("up", "tween", 0.2, 1)}
-        className="mt-[8px] font-normal text-[14px] md:text-[24px] text-center text-secondary-white dark:text-white"
+      <motion.div
+        variants={staggerContainer}
+        initial="hidden"
+        whileInView="show"
+        viewport={{ once: false, amount: 0.25 }}
+        className={`${styles.innerWidth} mx-auto ${styles.flexCenter} flex-col`}
       >
-        Quantitative results speak volumes
-      </motion.p>
-      <div className="relative h-[300px] pt-[150px]">
-        <div className="grid grid-cols-2 md:grid-cols-4 md:w-11/12 mx-auto pt-4 text-2xl">
+        <TypingText
+          title="| Metrics"
+          textStyles="text-center md:text-[30px] font-bold dark:text-gray-200"
+        />
+
+        <motion.p
+          variants={fadeIn("up", "tween", 0.2, 1)}
+          className="mt-[8px] font-normal text-[14px] md:text-[24px] text-center text-secondary-white dark:text-white"
+        >
+          Quantitative results speak volumes
+        </motion.p>
+        <div className="grid grid-cols-2 md:grid-cols-4 md:w-11/12 mx-auto pt-4 text-2xl mt-16 md:mt-32">
           <div className="text-center">
             <CountUp end={10} enableScrollSpy />
             <span className="inline">+</span>
@@ -53,7 +52,6 @@ const Numbers = () => {
             <h5 className="">Awards & Recognition</h5>
           </div>
         </div>
-      </div>
       </motion.div>
     </section>
   );
