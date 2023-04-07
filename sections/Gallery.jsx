5cdcf074e3780f69733d1004e651ec9b309e7879
Segun Ajibola <ajbl.segun@gmail.com>
2023-04-07 17:44:14 +0100

    service

--- sections/Gallery.jsx
+++ sections/Gallery.jsx
@@ -2,7 +2,6 @@ import React from "react";
 import Image from "next/image";
 import styles from "../styles";
 
-
 const Gallery = () => {
   return (
     <section
@@ -10,24 +9,29 @@ const Gallery = () => {
     >
       <div className="grid grid-cols-2 grid-rows-5 md:grid-cols-3 md:grid-rows-3 gap-3">
         <div className="col-span-1">
-          <div className="bg-red-500 w-full h-full">
-            Hello
+          <div className="w-full h-full">
+            <Image
+              scr="/decoration.jpg"
+              width={400}
+              height={400}
+              className="rounded-t-2xl object-cover object-center w-full h-full"
+            />
           </div>
         </div>
         <div className="col-span-1 row-span-2 md:row-span-1">
-          <div className="bg-green-500 w-full h-full"></div>
+          <div className="w-full h-full"></div>
         </div>
         <div className="col-span-1 md:row-span-2">
-          <div className="bg-yellow-500 w-full h-full"></div>
+          <div className="w-full h-full"></div>
         </div>
         <div className="col-span-1 md:col-span-2">
-          <div className="bg-blue-500 w-full h-full"></div>
+          <div className="w-full h-full"></div>
         </div>
         <div className="col-span-1">
-          <div className="bg-purple-500 w-full h-full"></div>
+          <div className="w-full h-full"></div>
         </div>
         <div className="col-span-2 md:col-span-2">
-          <div className="bg-orange-500 w-full h-full"></div>
+          <div className="w-full h-full"></div>
         </div>
       </div>
     </section>
