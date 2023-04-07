d6c66a752dd4d0028edce84fb817a9c90015e114
Segun Ajibola <ajbl.segun@gmail.com>
2023-04-07 17:25:16 +0100

    service

--- sections/Gallery.jsx
+++ sections/Gallery.jsx
@@ -4,10 +4,22 @@ import Image from "next/image";
 const Gallery = () => {
   return (
     <div className="grid grid-cols-2 grid-rows-5 md:grid-cols-3 md:grid-rows-2">
-      <div className="">
+      <div className="col-span-1">
         <Image />
       </div>
-      <div>
+      <div className="col-span-1 row-span-2">
+        <Image />
+      </div>
+      <div className="col-span-1">
+        <Image />
+      </div>
+      <div className="col-span-1">
+        <Image />
+      </div>
+      <div className="col-span-1">
+        <Image />
+      </div>
+      <div className="col-span-2">
         <Image />
       </div>
     </div>
