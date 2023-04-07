2566f8ff24db4ac41ac8601c59c461626114a862
Segun Ajibola <ajbl.segun@gmail.com>
2023-04-07 16:58:50 +0100

    service

---
+++ sections/Gallery.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Image from "next/image"
+
+const Gallery = () => {
+  return (
+    <div className='grid'>
+        <Image></Image>
+    </div>
+  )
+}
+
+export default Gallery
