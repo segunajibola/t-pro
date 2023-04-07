b3173c7aeb3ca79179300698e2659b11d0c2028f
Segun Ajibola <ajbl.segun@gmail.com>
2023-04-07 17:30:12 +0100

    service

--- sections/Gallery.jsx
+++ sections/Gallery.jsx
@@ -3,24 +3,24 @@ import Image from "next/image";
 
 const Gallery = () => {
   return (
-    <div className="grid grid-cols-2 grid-rows-5 md:grid-cols-3 md:grid-rows-2">
+    <div className="grid grid-cols-2 grid-rows-5 md:grid-cols-3 md:grid-rows-3 gap-3">
       <div className="col-span-1">
-        <Image />
+        <div className="bg-red-500 w-full"></div>
       </div>
-      <div className="col-span-1 row-span-2">
-        <Image />
+      <div className="col-span-1 row-span-2 md:row-span-1">
+        <div className="bg-green-500 w-full"></div>
+      </div>
+      <div className="col-span-1 md:row-span-2">
+        <div className="bg-yellow-500 w-full"></div>
+      </div>
+      <div className="col-span-1 md:col-span-2">
+        <div className="bg-blue-500 w-full"></div>
       </div>
       <div className="col-span-1">
-        <Image />
+        <div className="bg-purple-500 w-full"></div>
       </div>
-      <div className="col-span-1">
-        <Image />
-      </div>
-      <div className="col-span-1">
-        <Image />
-      </div>
-      <div className="col-span-2">
-        <Image />
+      <div className="col-span-2 md:col-span-2">
+        <div className="bg-orange-500 w-full"></div>
       </div>
     </div>
   );
