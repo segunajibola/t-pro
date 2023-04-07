9f98d1ae9af16d48c1610685f4ca1ba0808c0aba
Segun Ajibola <ajbl.segun@gmail.com>
2023-04-07 17:01:11 +0100

    service

--- sections/Gallery.jsx
+++ sections/Gallery.jsx
@@ -1,12 +1,17 @@
-import React from 'react'
-import Image from "next/image"
+import React from "react";
+import Image from "next/image";
 
 const Gallery = () => {
   return (
-    <div className='grid'>
-        <Image></Image>
+    <div className="grid grid-cols-2 grid-rows-5 md:grid-cols-3 md:grid-rows-2">
+      <div className="">
+        <Image />
+      </div>
+      <div>
+        <Image />
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Gallery
+export default Gallery;
