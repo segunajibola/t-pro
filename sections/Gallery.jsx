33062106ba9e38b963769a25d282ef70f4f5b8d1
Segun Ajibola <ajbl.segun@gmail.com>
2023-04-07 18:16:58 +0100

    service

--- sections/Gallery.jsx
+++ sections/Gallery.jsx
@@ -4,7 +4,6 @@ import styles from "../styles";
 import { motion } from "framer-motion";
 import { TypingText, ServiceCard } from "../components";
 import "../styles/gradient.css";
-import services from "../data/services";
 import { fadeIn, staggerContainer } from "../utils/motion";
 
 
@@ -13,6 +12,25 @@ const Gallery = () => {
     <section
       className={`${styles.paddings} md:py-24 bg-gray-200 dark:bg-gray-600 relative z-10`}
     >
+    <div className="gradient-02 z-0" />
+    <motion.div
+      variants={staggerContainer}
+      initial="hidden"
+      whileInView="show"
+      viewport={{ once: false, amount: 0.25 }}
+      className={`${styles.innerWidth} mx-auto ${styles.flexCenter} flex-col`}
+    >
+        <TypingText
+        title="| Gallery"
+        textStyles="text-center md:text-[30px] font-bold dark:text-gray-200"
+      />
+
+      <motion.p
+        variants={fadeIn("up", "tween", 0.2, 1)}
+        className="mt-[8px] font-normal text-[14px] md:text-[24px] text-center text-secondary-white dark:text-white"
+      >
+        Explore Our Creations
+      </motion.p>
       <div className="grid grid-cols-2 grid-rows-5 md:grid-cols-3 md:grid-rows-3 gap-4">
         <div className="col-span-1">
           <div className="w-full h-full">
@@ -87,6 +105,7 @@ const Gallery = () => {
           </div>
         </div>
       </div>
+    </motion.div>
     </section>
   );
 };
